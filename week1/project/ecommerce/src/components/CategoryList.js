7c5828a5d5bd35d597b3139561233ad1385e82ca
Chris <fayadchristina@gmail.com>
2022-02-10 12:18:16 +0100

    Passing stateCaegory to CategoryList not just ProductList

--- week1/project/ecommerce/src/components/CategoryList.js
+++ week1/project/ecommerce/src/components/CategoryList.js
@@ -2,19 +2,17 @@ import React from "react";
 import categories from "../fake-data/all-categories";
 import Button from "./Button";
 
-export default function CategoryList({ setCategory }) {
+export default function CategoryList({ stateCategory, setCategory }) {
   return (
     <div className="categories">
-      {categories.map((category, index) => {
-        return (
-          <Button
-            key={index}
-            index={index}
-            category={category}
-            setCategory={setCategory}
-          />
-        );
-      })}
+      {categories.map((category, index) => (
+        <Button
+          key={index}
+          stateCategory={stateCategory}
+          category={category}
+          setCategory={setCategory}
+        />
+      ))}
     </div>
   );
 }
