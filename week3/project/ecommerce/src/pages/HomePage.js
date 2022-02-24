c49bde834ab41b73a29a3d5efd37e510457d6bc3
Chris <fayadchristina@gmail.com>
2022-02-24 17:45:57 +0100

    Solving Ecommerce Shop III

--- week3/project/ecommerce/src/pages/HomePage.js
+++ week3/project/ecommerce/src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Header from "../components/Header";
 import CategoryList from "../components/CategoryList";
 import ProductList from "../components/ProductList";
 
@@ -6,6 +7,7 @@ export default function HomePage() {
   const [category, setCategory] = useState(null);
   return (
     <div className="App">
+      <Header title="Products" />
       <CategoryList category={category} setCategory={setCategory} />
       <ProductList category={category} />
     </div>
